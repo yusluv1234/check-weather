57366743213a7ca8567aab97dfd985335590156d
Yusluv <66624251+yusluv1234@users.noreply.github.com>
2021-03-21 21:31:15 +0100

    just pushing this

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { FetchWeather } from './api/FetchWether';
+import React, { useState } from 'react';
+import { fetchWether } from './api/FetchWether';
 import './App.css';
 
-
 const App = () => {
+  const [query, setQuery] = useState('');
+
+  const search = async (e) => {
+    if(e.key === 'Enter') {
+      const data = await fetchWether(query)
+      console.log(data);
+    }
+  }
+
   return (
     <div className="main-container">
-      <FetchWeather />
+      <input
+        type="text"
+        className="search"
+        placeholder="Search ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
     </div>
   )
 }
--- src/api/FetchWether.js
+++ src/api/FetchWether.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '401617f8e678722c870c0cf2334acbd1';
 
-export const FetchWether = async (query) => {
+export const fetchWether = async (query) => {
   const { data6 } = await axios.get(URL, {
     param: {
       q: query,
