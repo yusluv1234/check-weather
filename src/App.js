b6dbf042175085f6f670b6c29189fc034ee5e05d
yusluv1234 <yusluv124@gmail.com>
2021-04-29 23:08:53 +0100

    changing this app to typescript

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { useState } from 'react';
-import { fetchWeather } from './api/fetchWeather';
+import React from 'react';
 import './App.css';
 
 const App = () => {
-  const [query, setQuery] = useState('');
-
-  const search = async (e) => {
-    if(e.key === 'Enter') {
-      const data = await fetchWeather(query)
-      
-      console.log(data);
-    }
-  }
-
   return (
     <div className="main-container">
-      <input
-        type="text"
-        className="search"
-        placeholder="Search ..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
+        GoodMorning Yusuf Olododo
     </div>
   )
 }
