44dff7f88ebdd1f007240f070092dde131e6906e
yusluv1234 <yusluv124@gmail.com>
2021-04-30 03:18:25 +0100

    adding search, alert and store to index.js components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import '../node_modules/bulma/css/bulma.min.css';
 import App from './App';
+import store from './store';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<React.StrictMode>
+  <Provider store={store}>
+    <App /> 
+  </Provider>
+</React.StrictMode>, document.getElementById('root'));
